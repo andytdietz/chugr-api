044740b229f7323aa574e6062d4cc8b1e88b9649
andytdietz <andytdietz@gmail.com>
2024-04-18 22:38:17 -0600

    to be safe

--- app/views/breweries/_brewery.json.jbuilder
+++ app/views/breweries/_brewery.json.jbuilder
@@ -2,5 +2,4 @@ json.id brewery.id
 json.name brewery.name
 json.city brewery.city
 json.website_url brewery.website_url
-json.image_url brewery.image_url
 json.brewery_type brewery.brewery_type
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "example.com", "localhost:5173"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
+
+  allow do
+    origins "api.openbrewerydb.org"  # Add the API domain here
+    resource "*", headers: :any, methods: [:get]  # Allow only GET requests to the API
+  end
 end
